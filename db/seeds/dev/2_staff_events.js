1dd3062b6808bead24d05fc482bedd084437e5fa
Steven Lawson <unluckygamblermusic@gmail.com>
2018-05-23 16:37:11 -0600

    adding get endpoints for staff and events

--- db/seeds/dev/2_staff_events.js
+++ db/seeds/dev/2_staff_events.js
@@ -5,19 +5,19 @@ exports.seed = function(knex, Promise) {
       return knex('staff_events').insert([
         {
           event_id: 1,
-          staff_id: 4
+          staff_id: 1
         },
         {
           event_id: 1,
-          staff_id: 5
+          staff_id: 2
         },
         {
           event_id: 1,
-          staff_id: 6
+          staff_id: 3
         },
         {
           event_id: 2,
-          staff_id: 4
+          staff_id: 1
         }
       ]);
     });
--- server.js
+++ server.js
@@ -17,6 +17,72 @@ app.get('/', (request, response) => {
 
 });
 
+app.get('/api/v1/staff', (request, response) => {
+  database('staff').select()
+    .then(staff => {
+      response.status(200).json(staff)
+    })
+    .catch(error => {
+      response.status(500).json()
+    })
+})
+
+app.get('/api/v1/events', (request, response) => {
+  database('events').select()
+    .then(events => {
+      response.status(200).json(events)
+    })
+    .catch(error => {
+      response.status(500).json()
+    })
+})
+
+app.post('/api/v1/staff', (request, response) => {
+  const staff = request.body;
+  const keys = ['name', 'bartender', 'barback', 'bar_manager', 'ass_bar_manager', 'beer_bucket']
+  for (let requiredParameter of keys) {
+    console.log(staff[requiredParameter])
+    if (staff[requiredParameter] === undefined) {
+      return response
+        .status(422)
+        .send({
+          error: `You are missing a ${requiredParameter} property`
+        })
+    }
+  }
+
+  database('staff').insert(staff, keys)
+    .then(staff => {
+      response.status(201).json(staff[0])
+    })
+    .catch(error => {
+      response.status(500).json()
+    })
+})
+
+app.post('/api/v1/events', (request, response) => {
+  const events = request.body;
+  const keys = ['name', 'venue', 'date', 'time', 'bartenders', 'barbacks', 'bar_manager', 'ass_bar_manager', 'beer_bucket']
+  for (let requiredParameter of keys) {
+    console.log(events[requiredParameter])
+    if (events[requiredParameter] === undefined) {
+      return response
+        .status(422)
+        .send({
+          error: `You are missing a ${requiredParameter} property`
+        })
+    }
+  }
+
+  database('events').insert(events, keys)
+    .then(events => {
+      response.status(201).json(events[0])
+    })
+    .catch(error => {
+      response.status(500).json()
+    })
+})
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on port ${app.get('port')}`)
 })
