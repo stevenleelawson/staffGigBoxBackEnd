afb7fc86fc398c3664f9b151e8d3d788236c0c8c
Jared Eklin <jaredeklin@gmail.com>
2018-06-04 19:19:31 -0600

    Update seeds

--- db/seeds/dev/2_staff_events.js
+++ db/seeds/dev/2_staff_events.js
@@ -5,19 +5,33 @@ exports.seed = function(knex, Promise) {
       return knex('staff_events').insert([
         {
           event_id: 1,
-          staff_id: 1
+          staff_id: 1,
+          role: 'Bar Manager'
         },
         {
           event_id: 1,
-          staff_id: 2
+          staff_id: 2,
+          role: 'Bartender'
         },
         {
           event_id: 1,
-          staff_id: 3
+          staff_id: 3,
+          role: 'Barback'
         },
         {
           event_id: 2,
-          staff_id: 1
+          staff_id: 3,
+          role: 'Bar Manager'
+        },
+        {
+          event_id: 2,
+          staff_id: 4,
+          role: 'Bartender'
+        },
+        {
+          event_id: 2,
+          staff_id: 5,
+          role: 'Barback'
         }
       ]);
     });
