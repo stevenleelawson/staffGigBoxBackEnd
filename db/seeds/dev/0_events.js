c52e0c58b68b73fc5b4cb6f013c08c0698ba3cdc
Jared Eklin <jaredeklin@gmail.com>
2018-06-21 15:24:17 -0600

    Update dev seeds

--- db/seeds/dev/0_events.js
+++ db/seeds/dev/0_events.js
@@ -19,23 +19,23 @@ exports.seed = function(knex, Promise) {
           name: 'Slim Cessna',
           date: '05/02/18',
           time: '8:00 pm',
-          bartenders: 4,
+          bartenders: 3,
           barbacks: 1,
           bar_manager: true,
           ass_bar_manager: false,
           beer_bucket: false
         },
-        {
-          venue: 'Ogden',
-          name: 'Billy Prince Billy',
-          date: '05/12/18',
-          time: '6:00 pm',
-          bartenders: 8,
-          barbacks: 2,
-          bar_manager: true,
-          ass_bar_manager: true,
-          beer_bucket: true
-        }
+        // {
+        //   venue: 'Ogden',
+        //   name: 'Billy Prince Billy',
+        //   date: '05/12/18',
+        //   time: '6:00 pm',
+        //   bartenders: 8,
+        //   barbacks: 2,
+        //   bar_manager: true,
+        //   ass_bar_manager: true,
+        //   beer_bucket: true
+        // }
       ]);
     });
 };
--- db/seeds/dev/2_staff_events.js
+++ db/seeds/dev/2_staff_events.js
@@ -16,12 +16,32 @@ exports.seed = function(knex, Promise) {
         {
           event_id: 1,
           staff_id: 3,
+          role: 'Bartender'
+        },
+        {
+          event_id:1,
+          staff_id: 4,
+          role: 'Bartender'
+        },
+        {
+          event_id:1,
+          staff_id: 5,
+          role: 'Bartender'
+        },
+        {
+          event_id: 1,
+          staff_id: 6,
           role: 'Barback'
         },
         {
           event_id: 2,
-          staff_id: 3,
+          staff_id: 1,
           role: 'Bar Manager'
+        },
+        {
+          event_id: 2,
+          staff_id: 3,
+          role: 'Bartender'
         },
         {
           event_id: 2,
@@ -31,8 +51,73 @@ exports.seed = function(knex, Promise) {
         {
           event_id: 2,
           staff_id: 5,
+          role: 'Bartender'
+        },
+        {
+          event_id: 2,
+          staff_id: 12,
           role: 'Barback'
-        }
+        },
+        // {
+        //   event_id: 3,
+        //   staff_id: null,
+        //   role: 'Bar Manager'
+        // },
+        //         {
+        //   event_id: 3,
+        //   staff_id: null,
+        //   role: 'Assistant Bar Manager'
+        // },
+        // {
+        //   event_id: 3,
+        //   staff_id: null,
+        //   role: 'Bartender'
+        // },
+        // {
+        //   event_id: 3,
+        //   staff_id: null,
+        //   role: 'Bartender'
+        // },
+        // {
+        //   event_id: 3,
+        //   staff_id: null,
+        //   role: 'Bartender'
+        // },
+        // {
+        //   event_id: 3,
+        //   staff_id: null,
+        //   role: 'Bartender'
+        // },
+        // {
+        //   event_id: 3,
+        //   staff_id: null,
+        //   role: 'Bartender'
+        // },
+        // {
+        //   event_id: 3,
+        //   staff_id: null,
+        //   role: 'Bartender'
+        // },
+        // {
+        //   event_id: 3,
+        //   staff_id: null,
+        //   role: 'Bartender'
+        // },
+        // {
+        //   event_id: 3,
+        //   staff_id: null,
+        //   role: 'Bartender'
+        // },
+        // {
+        //   event_id: 3,
+        //   staff_id: null,
+        //   role: 'Barback'
+        // }, 
+        // {
+        //   event_id: 3,
+        //   staff_id: null,
+        //   role: 'Barback'
+        // }  
       ]);
     });
 };
